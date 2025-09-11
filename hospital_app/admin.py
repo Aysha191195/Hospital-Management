b03990484e82522d4c18b69e3105f5625205f963
Aysha Reshma <ayshareshma95@gmail.com>
2025-09-11 21:36:03 +0530

    updates admin file

--- hospital_app/admin.py
+++ hospital_app/admin.py
@@ -1,30 +1,34 @@
-
-# Register your models here.
-
 from django.contrib import admin
-from .models import Patient, Doctor, Appointment, Treatment, Bill
-
-@admin.register(Patient)
-class PatientAdmin(admin.ModelAdmin):
-    list_display = ('id','name','gender','dob','phone')
-    search_fields = ('name','phone','address')
+from .models import Doctor, Patient, Appointment, Treatment, Bill
 
 @admin.register(Doctor)
 class DoctorAdmin(admin.ModelAdmin):
-    list_display = ('id','name','specialization','phone','email')
-    search_fields = ('name','specialization')
+    list_display = ('name', 'specialization', 'phone')
+    search_fields = ('name', 'specialization', 'phone')
+
+
+@admin.register(Patient)
+class PatientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'gender', 'phone')
+    search_fields = ('name', 'phone')
+    list_filter = ('gender',)
+
 
 @admin.register(Appointment)
 class AppointmentAdmin(admin.ModelAdmin):
-    list_display = ('id','patient','doctor','appointment_date','diagnosis')
-    list_filter = ('doctor','appointment_date')
-    search_fields = ('patient__name','doctor__name','diagnosis')
+    list_display = ('patient', 'doctor', 'date', 'time')
+    list_filter = ('date', 'doctor')
+
 
 @admin.register(Treatment)
 class TreatmentAdmin(admin.ModelAdmin):
-    list_display = ('id','appointment','treatment_description','medicine_prescribed')
+    list_display = ('name', 'patient', 'doctor', 'date')
+    search_fields = ('name', 'patient__name', 'doctor__name')
+    list_filter = ('date',)
+
 
 @admin.register(Bill)
 class BillAdmin(admin.ModelAdmin):
-    list_display = ('id','appointment','total_amount','payment_status')
-    list_filter = ('payment_status',)
+    list_display = ('patient', 'amount', 'status', 'date')
+    search_fields = ('patient__name',)
+    list_filter = ('status', 'date')
